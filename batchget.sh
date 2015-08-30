6a019f9c0d07ee035287d43c8d807891c0309c38
Sergey Zemtsov <wbssergey@users.noreply.github.com>
2015-08-29 23:30:29 -0400

    Create batchget.sh

---
+++ batchget.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+ bash getparam.sh variants barcode
+ bash getparam.sh products body_html
+ bash getparam.sh pages author
+ bash getparam.sh collects collect
+ bash getparam.sh themes created
+ bash getparam.sh articles author
+ bash getparam.sh checkouts buyer
+ bash getparam.sh custom_collections body_html
+ bash getparam.sh countries code
+ bash getparam.sh assets key
+
+ exit 0;
+
+
